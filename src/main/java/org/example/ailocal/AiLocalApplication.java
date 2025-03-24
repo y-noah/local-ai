69d80c09182ccc516928187badc1e48648d57c36
y-noah <hy.nhzj@gmail.com>
2025-03-24 16:32:50 +0800

    init

---
+++ src/main/java/org/example/ailocal/AiLocalApplication.java
@@ -0,0 +1,13 @@
+package org.example.ailocal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiLocalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiLocalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ailocal/AiLocalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ailocal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiLocalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
